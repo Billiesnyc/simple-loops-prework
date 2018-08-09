098473082f20edc25acffb005cf97b8bc3a965a6
Billie Simmons <billiesnyc@gmail.com>
2018-08-09 17:11:45 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,7 @@ def loop_iterator(number_of_times)
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times.times 
   
 end
 
